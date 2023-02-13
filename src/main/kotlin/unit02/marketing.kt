a156b0af3cfedd949582c6d682f0f00dfe61db7a
SammaCo <otaku_sdnc@yahoo.com>
2023-02-12 23:44:00 -0600

    Assignment 02.3

---
+++ src/main/kotlin/unit02/marketing.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var friendsCount = 42
+    if (friendsCount < 500) {
+        println("Buy Cat Food at 20% Off!")
+    }
+    else{
+        println("Adopt a Dog Today!")
+    }
+}
