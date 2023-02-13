b106046e8e3c97aaed97f8a6a8829c0f48449e31
SammaCo <otaku_sdnc@yahoo.com>
2023-02-12 23:26:44 -0600

    Assignment 02.2

---
+++ src/main/kotlin/unit02/variables.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val myAge: Int = 33
+
+    var averageAge: Double = 33.0
+    averageAge = (averageAge + 30.0) /2
+    println("Average age is: " + averageAge)
+
+    val testNumber: Int = 73
+    val evenOdd = testNumber % 2
+    println("Remainder is: " + evenOdd)
+    //cannot be changed to other numbers as it is a constant
+
+    var answer = 0
+    answer ++
+    answer += 10
+    answer *= 10
+    answer *= 1000
+    println("Answer is: " + answer)
+}
