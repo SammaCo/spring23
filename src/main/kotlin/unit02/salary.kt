075b9160b44a8ef48d6b1310dfdde9d3f4806d57
SammaCo <otaku_sdnc@yahoo.com>
2023-02-13 00:01:40 -0600

    Assignment 02.4

---
+++ src/main/kotlin/unit02/salary.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var name = "Samantha Cohen"
+    var age = 33
+    var annualPay: Double = 1000000.00
+    println ("My name is $name, my age is $age and I hope to earn $$annualPay per year.")
+}
