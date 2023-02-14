b6e88ac18bf8ab52f2bd02caf6c122eb1c2938b3
SammaCo <otaku_sdnc@yahoo.com>
2023-02-13 23:38:35 -0600

    Assignment 02.8

---
+++ src/main/kotlin/unit02/stringMani.kt
@@ -0,0 +1,8 @@
+fun main(){
+    print("Enter a city: ")
+    var city = readLine()!!
+    println(city.length)
+    println(city.toUpperCase())
+    println(city.toLowerCase())
+    println(city[0])
+    }
